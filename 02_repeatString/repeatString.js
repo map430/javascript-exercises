51ef589ae4434aa3b3617a5322c66221928ce199
Michael Paladino <128327772+map430@users.noreply.github.com>
2023-04-15 23:48:10 -0400

    Establish code to pass five tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,29 @@
-const repeatString = function() {
-
+const repeatString = function(myString, n) {
+    let repeatedString = '';
+    if (n < 0){
+        return('ERROR');
+    }else {
+    }for (let i=0; i<n; i++) {
+    repeatedString += myString;
+}
+return(repeatedString);
 };
+
+repeatString('hey', 3);
+
+repeatString('hey', 10);
+
+repeatString('hey', 1);
+
+repeatString('hey', 0);
+
+repeatString('hey', -1);
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;

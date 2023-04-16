d5f0301cf35151edb48d20f7c09b6d52436d38b3
Michael Paladino <128327772+map430@users.noreply.github.com>
2023-04-16 17:18:54 -0400

    Establish code to solve all 6 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,12 @@
-const removeFromArray = function(arr) {
-    return arr.toString();
-    };
-
-    console.log(removeFromArray(["banana", "orange", "apple"]));
+function removeFromArray(arr, ...nums) {
+    nums.forEach(function(num) {
+      let index = arr.indexOf(num);
+      if (index !== -1) {
+        arr.splice(index, 1);
+      }
+    });
+    return arr;
+  }
 
 // Do not edit below this line
 module.exports = removeFromArray;

a2cbfe2280407d80e3ed39aed89da7b1d48dc337
Michael Paladino <128327772+map430@users.noreply.github.com>
2023-04-16 13:26:48 -0400

    Build code to pass test 1

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function(myString) {
+    let stringLen = myString.length;
+    let revString = '';
+    for (let i = stringLen; i >= 0; i--) {
+       let indivLetter = myString.slice(i-1, i);
+       revString += indivLetter
+    }
+    return revString;
+   };
+   
+   reverseString('hello');
 
-};
 
+//create a loop that takes a string and analyzes each letter starting from the -1 postion
+    //start at -1 and slice out i, then return it
+    //go to -2 then slice out i
+  //return i each time 
 // Do not edit below this line
 module.exports = reverseString;

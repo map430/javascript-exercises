ac8dff8b49d6608c8bc9fc7dd51fb3c563ca3384
Michael Paladino <128327772+map430@users.noreply.github.com>
2023-04-16 13:31:07 -0400

    Establish code passes all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -10,10 +10,12 @@ const reverseString = function(myString) {
    
    reverseString('hello');
 
+   reverseString('hello there');
 
-//create a loop that takes a string and analyzes each letter starting from the -1 postion
-    //start at -1 and slice out i, then return it
-    //go to -2 then slice out i
-  //return i each time 
-// Do not edit below this line
+   reverseString('123! abc!');
+
+   reverseString('');
+
+
+
 module.exports = reverseString;

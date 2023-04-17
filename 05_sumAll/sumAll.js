ded369d374124de1904d047f15029fd4ebaacdeb
Michael Paladino <128327772+map430@users.noreply.github.com>
2023-04-17 16:18:16 -0400

    Establish loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    let finalSum;
+    for (i=min; i<=max; i++) {
+      console.log(i);
+    }
 };
+
+sumAll(1,4);
+
+
+/*
+Sart a loop
+    Begins at the min
+    Iterates as long as i is less than or equal to the max
+    increments by ++
+As each number exits the loop, convert it to a number and add it to an empty variable 
+Return the variable 
+Close the function 
+
+*/
+
 
 // Do not edit below this line
 module.exports = sumAll;

9dcfb69ab3a6c9b92bf5df4f47ad407de72e301c
Michael Paladino <128327772+map430@users.noreply.github.com>
2023-04-17 16:26:28 -0400

    Establish code to past first test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,23 +1,11 @@
 const sumAll = function(min, max) {
-    let finalSum;
+    let finalSum = 0;
     for (i=min; i<=max; i++) {
-      console.log(i);
+      finalSum += i;
     }
+   return finalSum;
 };
 
-sumAll(1,4);
-
-
-/*
-Sart a loop
-    Begins at the min
-    Iterates as long as i is less than or equal to the max
-    increments by ++
-As each number exits the loop, convert it to a number and add it to an empty variable 
-Return the variable 
-Close the function 
-
-*/
 
 
 // Do not edit below this line

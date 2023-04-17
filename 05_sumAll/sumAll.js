b496673916ac2b384030b349603226c8536808b7
Michael Paladino <128327772+map430@users.noreply.github.com>
2023-04-17 17:26:56 -0400

    Modify if statement to satisfy 5th test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(val1, val2) {
     let finalSum = 0;
-    if (val1 < 0 || val2 <0) {
+    if ((val1 < 0 || val2 <0) || typeof(val1) !== "number" || typeof(val2) !== "number") {
         return "ERROR";
     }else if (val1 > val2) {
         for (i=val2; i<=val1; i++) {

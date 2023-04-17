c9426ef9375e47447cec62d7f08bd32e4dba573c
Michael Paladino <128327772+map430@users.noreply.github.com>
2023-04-17 16:50:10 -0400

    Establish code to pass third test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,16 @@
-const sumAll = function(min, max) {
+const sumAll = function(val1, val2) {
     let finalSum = 0;
-    for (i=min; i<=max; i++) {
-      finalSum += i;
+    if (val1 > val2) {
+        for (i=val2; i<=val1; i++) {
+          finalSum += i;
+        }
+    }else if (val2 > val1) {
+        for (i=val1; i<=val2; i++) {
+          finalSum += i;
+        }
     }
    return finalSum;
 };
-
-
 
 // Do not edit below this line
 module.exports = sumAll;

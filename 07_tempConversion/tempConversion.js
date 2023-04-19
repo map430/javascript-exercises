db202637b2e2a5bb4a4f23f475bfe10472ab8db4
Michael Paladino <128327772+map430@users.noreply.github.com>
2023-04-19 18:40:19 -0400

    Establish code to pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = Math.round(((tempF - 32) * (5 / 9) * 10)) / 10;
+  return tempC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = Math.round((tempC * (9/5) + 32) * 10) / 10;
+  return tempF;
 };
+
+
 
 // Do not edit below this line
 module.exports = {

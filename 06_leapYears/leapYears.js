c7cff43821212ff9ed095bc0e7195a8f7102d4c3
Michael Paladino <128327772+map430@users.noreply.github.com>
2023-04-18 21:18:21 -0400

    Establish code to pass all six tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 100 !== 0 && year % 4 === 0) {
+        return true; 
+    }else if (year % 100 === 0 && year % 400 === 0) {
+        return true;
+    }else {
+        return false;
+    }  
 };
+
+
+/*
+
+Leap years are divisible by four
+Years divisible by 100 are not leap years unless they are divisible by 400
+
+
+*/
+
+
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
